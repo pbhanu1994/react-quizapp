d93cd3ac058a5b7d82938fd924756174f0b5e4e7
Bhanu <pbhanu.1994@gmail.com>
2019-03-15 03:15:29 +1100

    Added Copyrights to homescreen

---
+++ src/components/common/copyrights.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Copyrights = () => {
+  return (
+    <footer>
+      <div id="copyrights" style={{ paddingTop: 240, paddingBottom: 0 }}>
+        <hr />
+        <center>
+          <p style={{ color: "lightslategrey" }}> &copy; BHANU PRAKASH</p>
+        </center>
+      </div>
+    </footer>
+  );
+};
+
+export default Copyrights;
